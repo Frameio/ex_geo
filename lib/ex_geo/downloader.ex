0db35000c557510a194f88c1b90c14cc6e36454e
Zach Smith <zd@zdsmith.com>
2020-01-02 12:54:38 -0500

    Accept helper callback to handle new MMDB format

--- lib/ex_geo/downloader.ex
+++ lib/ex_geo/downloader.ex
@@ -8,11 +8,28 @@ defmodule ExGeo.Downloader do
   """
   @spec download!(binary) :: binary
   def download!(url, retry \\ 1)
-  def download!(_, retry) when retry >= 3, do: raise DownloadError, message: "retries exceeded"
+  def download!(_, retry) when retry >= 3, do: raise(DownloadError, message: "retries exceeded")
+
   def download!(url, retry) do
     case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} -> body
       _ -> download!(url, retry + 1)
     end
+  end
+end
+
+defmodule ExGeo.Downloader.MaxmindHelper do
+  def handle(data) do
+    unzipped = :zlib.gunzip(data)
+    {:ok, contents} = :erl_tar.extract({:binary, unzipped}, [:memory])
+
+    {_, db} =
+      Enum.find(contents, fn {filename, _} ->
+        filename
+        |> List.to_string()
+        |> String.ends_with?(".mmdb")
+      end)
+
+    db
   end
 end
